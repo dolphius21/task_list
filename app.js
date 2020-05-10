2dc3f21fce2803a0abd5bfb5bee62719cdfc7b3c
dolphius21 <johnrudolphlee@gmail.com>
2020-05-10 18:23:30 +0800

    remove task function

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
 }
 
 // Add Task
@@ -39,4 +41,13 @@ function addTask(e) {
   taskInput.value = '';
   // Prevent default behavior
   e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
