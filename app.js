630c514d48862e220062f939d9bc338495fccf38
dolphius21 <johnrudolphlee@gmail.com>
2020-05-10 18:51:54 +0800

    filter tasks function

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // Clear task event
   clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -61,4 +63,17 @@ function clearTasks() {
   while (taskList.children) {
     taskList.removeChild();
   }
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
